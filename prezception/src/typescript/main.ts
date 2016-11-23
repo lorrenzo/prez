72077ee550047229005269e34180d09ade5bc4a9
Rémi Nonnon <remi.nonnon@carboatmedia.fr>
2016-11-23 10:45:06 +0100

    Revert "revealjs"
    This reverts commit 8de586f623a18706f517242547894b53c1220d29.

--- prezception/src/typescript/main.ts
+++ prezception/src/typescript/main.ts
@@ -1,4 +1,10 @@
+import * as Reveal from "reveal";
 import * as _ from 'lodash';
 
 let names = ["toto", "titi", "tata", "tutu"];
 _.map(names, (name) => console.log(name));
+
+Reveal.initialize({
+    progress: true,
+    history: true
+});
