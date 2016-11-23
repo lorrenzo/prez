379f9b4dd153e00c304095204d4f47c9146ac18f
Rémi Nonnon <remi.nonnon@carboatmedia.fr>
2016-11-23 10:45:07 +0100

    Revert "less"
    This reverts commit 812470b5d84fa4aa1556a750e442fa3379b867ed.

--- prezception/webpack.config.js
+++ prezception/webpack.config.js
@@ -1,8 +1,12 @@
 'use strict';
-var path = require("path");
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var extractLESS = new ExtractTextPlugin('styles/main.css');
+
 module.exports = {
     entry: [
-        './src/typescript/main.ts'
+        './src/typescript/main.ts',
+        "./src/styles/main.less"
     ],
     output: {
         path: './src/dist',
@@ -11,7 +15,11 @@ module.exports = {
     module: {
         rules: [
             { test: /\.ts$/, use: 'ts-loader' },
-            { test: /.(png|woff(2)?|eot|ttf|svg|ico)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000&name=images/[name].[ext]' }
+            {
+                test: /\.less$/,
+                loader: extractLESS.extract(['css-loader','less-loader'])
+            },
+            { test: /.(png|woff(2)?|eot|ttf|svg|ico)(\?[a-z0-9=\.]+)?$/, loader: 'url-loader?limit=100000&name=images/[name].[ext]' },
         ]
     },
 
@@ -23,5 +31,8 @@ module.exports = {
         // Add '.ts' and '.tsx' as a resolvable extension.
         extensions: [ ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
     },
+    plugins: [
+        extractLESS
+    ],
     devtool: false
 };
