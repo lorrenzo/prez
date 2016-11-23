8de586f623a18706f517242547894b53c1220d29
Rémi Nonnon <remi.nonnon@carboatmedia.fr>
2016-11-23 10:21:18 +0100

    revealjs

--- prezception/src/typescript/main.ts
+++ prezception/src/typescript/main.ts
@@ -1,10 +1,4 @@
-import * as Reveal from "reveal";
 import * as _ from 'lodash';
 
 let names = ["toto", "titi", "tata", "tutu"];
 _.map(names, (name) => console.log(name));
-
-Reveal.initialize({
-    progress: true,
-    history: true
-});
